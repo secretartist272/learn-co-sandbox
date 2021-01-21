94fc3856b36400cecc9ce9feb45f1fa30e57c59a
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-21 21:47:25 +0000

    Automatically backed up by Learn

---
+++ loops.rb
@@ -0,0 +1,10 @@
+
+
+counter = 0
+while counter < 20
+  
+  puts "The #{current_number} is less than 20."
+  
+end
+
+current_number = (counter + 1)
---
+++ prework/learning-def-end.rb
@@ -0,0 +1,9 @@
+def greeting # type this first
+  puts "Hello World
+end
+
+greeting # Executing the method by name 
+#=> "Hello World"
+
+greeting #Executing the method again
+#=> "Hello World"
